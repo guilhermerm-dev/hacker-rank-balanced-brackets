b1640bed44888a6b4cfd9cd4cc6eb4e9c6571860
Guilherme Ramos Mendes <guilhermerm.ciandt@sulamerica.com.br>
2020-12-11 23:20:39 -0300

    first commit

---
+++ hacker-rank-balanced-brackets/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hacker_rank_balanced_brackets
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> charSequences = new List<string> { "{[()]}", "{[(])}", "{{[[(())]]}}" };
+            charSequences.ForEach(sequence => Console.WriteLine(IsBalanced(sequence)));
+        }
+
+        private static string IsBalanced(string s)
+        {
+            char[] brackets = s.ToCharArray();
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < brackets.Length; i++)
+            {
+                if (AreOpeningBrackets(brackets[i]))
+                {
+                    stack.Push(brackets[i]);
+                }
+                else
+                {
+                    if (stack.IsEmpty())
+                    {
+                        return "NO";
+                    }
+                    else
+                    {
+                        char popValue = stack.Pop();
+                        if (brackets[i] == '}' && popValue != '{')
+                        {
+                            return "NO";
+                        }
+                        else if (brackets[i] == ')' && popValue != '(')
+                        {
+                            return "NO";
+                        }
+                        else if (brackets[i] == ']' && popValue != '[')
+                        {
+                            return "NO";
+                        }
+                    }
+                }
+            }
+
+            if (stack.Count == 0)
+            {
+                return "YES";
+            }
+            else
+            {
+                return "NO";
+            }
+        }
+
+        private static bool AreOpeningBrackets(char value)
+        {
+            return (value == '{' || value == '(' || value == '[');
+        }
+    }
+}
---
+++ hacker-rank-balanced-brackets/StackExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace hacker_rank_balanced_brackets
+{
+    public static class StackExtensions
+    {
+        public static bool IsEmpty(this Stack<char> stack)
+        {
+            if (stack is null)
+                return false;
+
+            return stack.Count == 0;
+        }
+    }
+}
